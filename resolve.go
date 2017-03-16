89f11efcd527314f9556856d972a3755ecf4cfc9
headz <sean@dulotech.com>
2017-03-16 17:48:04 -0400

    Formatting

--- resolve.go
+++ resolve.go
@@ -2,7 +2,6 @@ package dbsampler
 
 import (
 	"github.com/deckarep/golang-set"
-	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -22,37 +21,26 @@ func resolveTableGraph(graph TableGraph) (TableGraph, error) {
 		tableNames[table.Name] = table
 		tableDeps[table.Name] = depSet
 	}
-
-	// Iteratively find and remove nodes from the graph which have no dependencies.
-	// If at some point there are still nodes in the graph and we cannot find
-	// nodes without dependencies, that means we have a circular dependency
+	
 	var resolved TableGraph
 	for len(tableDeps) != 0 {
-		// Get all nodes from the graph which have no dependencies
 		readySet := mapset.NewSet()
 		for tableName, deps := range tableDeps {
 			if deps.Cardinality() == 0 {
 				readySet.Add(tableName)
 			}
 		}
-
-		// If there aren't any ready nodes, then we have a circular dependency
 		if readySet.Cardinality() == 0 {
-			var g TableGraph
+			s := []string{}
 			for tableName := range tableDeps {
-				g = append(g, tableNames[tableName])
+				s = append(s, "`"+tableName+"`")
 			}
-			return g, errors.New("Circular dependency found")
+			return resolved, fmt.Errorf("Circular dependency found -> %s", strings.Join(s, ", "))
 		}
-
-		// Remove the ready nodes and add them to the resolved graph
 		for tableName := range readySet.Iter() {
 			delete(tableDeps, tableName.(string))
 			resolved = append(resolved, tableNames[tableName.(string)])
 		}
-
-		// Also make sure to remove the ready nodes from the
-		// remaining node dependencies as well
 		for tableName, deps := range tableDeps {
 			diff := deps.Difference(readySet)
 			tableDeps[tableName] = diff
