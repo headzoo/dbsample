54f52a50737c7dd705940ebc10529eb75f9daf4e
headz <sean@dulotech.com>
2017-03-17 21:47:31 -0400

    Updates the build script

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ fileb0x templates/b0x.yaml
 go build -o dist/dbsample cli/main.go
 
 TAG=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
-tar -cvzf dist/dbsample-${TAG}.linux-amd64.tar.gz -C dist dbsample
+tar -cvzf dist/dbsample-${TAG}-linux-amd64.tar.gz -C dist dbsample
