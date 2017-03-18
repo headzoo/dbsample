45fbe3c188e487fe0f068982cd17aa422c7f045c
headz <sean@dulotech.com>
2017-03-17 22:19:30 -0400

    Adds appveyor.yml

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+TAG=$(git describe --tags --abbrev=0)
 fileb0x templates/b0x.yaml
-go build -o dist/dbsample cli/main.go
-
-TAG=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
+GOOS="linux" GOARCH="amd64" go build -o dist/dbsample cli/main.go
 tar -cvzf dist/dbsample-${TAG}-linux-amd64.tar.gz -C dist dbsample
+
+GOOS="linux" GOARCH="386" go build -o dist/dbsample cli/main.go
+tar -cvzf dist/dbsample-${TAG}-linux-386.tar.gz -C dist dbsample
